7f4701ac23238b3596f6d4d6c70c7451c34d1997
PierreLucSavoie <savoieinvesting@gmail.com>
2022-04-15 12:57:47 -0300

    Created Book model
    book contains
    -id
    -title
    -year published
    -user id

---
+++ Models/Book.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookDatabase.Models
+{
+    public class Book
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public int Year { get; set; }
+        [Required]
+        public string Author { get; set; }
+
+        //will implement image later
+        //public string? Image { get; set; }
+
+        //will be used to identify if book belongs to specific user
+        public int UserId { get; set; }
+    }
+}
