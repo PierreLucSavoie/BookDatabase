789f4dbd36015a4e7e40efde1a75a71f8a7caaca
PierreLucSavoie <savoieinvesting@gmail.com>
2022-04-15 12:17:53 -0300

    Database Initialisation
    -Changed name of database
    -removed email confirmation for new users
    -Updated database

--- Program.cs
+++ Program.cs
@@ -10,7 +10,8 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+//Disabled email confirmation for new account
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false)
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
 
